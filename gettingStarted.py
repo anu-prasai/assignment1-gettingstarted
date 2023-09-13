b50265c84ce9e1cae507c741800904329f3a515c
anuprasai <ap8691@nyu.edu>
2023-09-13 18:37:51 -0500

    Adding answer

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,7 +1,7 @@
 ### welcome_assignment_answers
 ### Input - All nine questions given in the assignment.
 ### Output - The right answer for the specific question.
-
+import hashlib
 def welcome_assignment_answers(question):
     # Students do not have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
@@ -11,21 +11,23 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == ("In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel "
                       "posted by a TA?"):
-        answer = "pacp"
+        answer = "pcap"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = "No"
+        email = "ap8691@nyu.edu"
+        m = hashlib.sha256(email.encode('UTF-8'))
+        answer = m.hexdigest()
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == ("What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an "
                       "integer number"):
-        answer = "No"
+        answer = int(5)
     elif question == ("What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an "
                       "integer number"):
-        answer = "No"
+        answer = int(4)
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -39,7 +41,8 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = ("What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an "
+                      "integer number")
     print(welcome_assignment_answers(debug_question))
 
 # Questions:
